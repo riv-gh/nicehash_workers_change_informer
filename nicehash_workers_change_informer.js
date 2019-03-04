80da3c1a81d26b51ac4bb58ae0b45743be5cdd3d
daenur <daenur@outlook.com>
2019-03-04 10:31:08 +0200

    Update nicehash_workers_change_informer.js

--- nicehash_workers_change_informer.js
+++ nicehash_workers_change_informer.js
@@ -1,7 +1,12 @@
 const BTC_WALET = '';
-const TELEGRAM_MESSAGE_URL = 'https://api.telegram.org/bot<token>/sendMessage?chat_id=<chat_id>&text='; 
-// https://core.telegram.org/bots/api
+const TELEGRAM_BOT_TOKEN = '';
+const TELEGRAM_CHAT_ID = '';
+const CHECK_INTERVAL = 10000;
+const NICEHASH_API_LINK = 'https://api.nicehash.com/api?method=stats.provider.workers&addr='+BTC_WALET;
+const TELEGRAM_MESSAGE_URL = 'https://api.telegram.org/bot'+TELEGRAM_BOT_TOKEN+'/sendMessage?chat_id='+TELEGRAM_CHAT_ID+'&text=';
 let lastWorkersCount = 0;
+
+"https://api.nicehash.com"!=document.location.origin&&(document.location.href=NICEHASH_API_LINK);
 
 function getJSON(url, callback) {
   let xhr = new XMLHttpRequest();
@@ -24,23 +29,26 @@ function sendTelegramMessage(message) {
   setTimeout(function(){tlegramWindow.close();},5000);
 }
 
-getJSON('https://api.nicehash.com/api?method=stats.provider.workers&addr='+BTC_WALET,
-function(err, data) {
-  if (err !== null) {
-    console.error('Something went wrong: ' + err);
-  } else {
-    console.info('Your workers count: ' + data.result.workers.length);
-    let workersCount = data.result.workers.length;
-    if (workersCount!=lastWorkersCount) {
-      let message = "Workers count change!\r\n"+
-                    (workersCount<lastWorkersCount?"Some workers are lost...":"Some workers are recovery")+
-                    " \r\nActive workers:\r\n";
-      for (let i=0; i<data.result.workers.length; i++) {
-        message+=""+(i+1)+". "+(!data.result.workers[i][0].length?"[no nane]":data.result.workers[i][0])+"\r\n";
+function checkNicehash() {
+getJSON(NICEHASH_API_LINK,
+  function(err, data) {
+    if (err !== null) {
+      console.error('Something went wrong: ' + err);
+    } else {
+      console.info('Your workers count: ' + data.result.workers.length);
+      let workersCount = data.result.workers.length;
+      if (workersCount!=lastWorkersCount) {
+        let message = "Workers count change!\r\n"+
+                      (workersCount<lastWorkersCount?"Some workers are lost...":"Some workers are recovery")+
+                      " \r\nActive workers:\r\n";
+        for (let i=0; i<data.result.workers.length; i++) {
+          message+=""+(i+1)+". "+(!data.result.workers[i][0].length?"[no nane]":data.result.workers[i][0])+"\r\n";
+        }
+        console.info(message);
+        sendTelegramMessage(message);
+        lastWorkersCount = workersCount;
       }
-      console.info(message);
-      sendTelegramMessage(message);
-      lastWorkersCount = workersCount;
     }
-  }
-});
+  });
+  setTimeout(ceckNicehash,CHECK_INTERVAL);
+}
